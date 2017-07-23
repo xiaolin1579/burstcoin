9130863b4dc1a637db3811a7617e40a1ac059a3f
MSFTserver <sawyerlemmo@gmail.com>
2017-07-23 06:35:30 -0700

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,12 +1,12 @@
 CP=conf/:classes/:lib/*
 SP=src/java/
-
+echo "burst.jar Compiling Initiated."
+echo "classes Compiling Started.."
 /bin/mkdir -p classes/
-
 javac -sourcepath $SP -classpath $CP -d classes/ src/java/nxt/*.java src/java/nxt/*/*.java src/java/fr/cryptohash/*.java || exit 1
-
+echo "classes generated successfully..."
+echo "burst.jar Compiling Started...."
 /bin/rm -f burst.jar 
 jar cf burst.jar -C classes . || exit 1
 /bin/rm -rf classes
-
-echo "burst.jar generated successfully"
+echo "burst.jar generated successfully!"
